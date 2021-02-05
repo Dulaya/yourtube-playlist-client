509447441c899ff017aea6ab920a214fecc279ea
Dulaya Saennok <dulaya.saennok@gmail.com>
2021-02-05 02:37:50 -0800

    update url matching Heroku

--- src/urls/urls.js
+++ src/urls/urls.js
@@ -1,10 +1,10 @@
 
 
 
-export const pictureURL = 'https://your-tube-playlist.herokuapp.com/posts';
+export const pictureURL = 'https://your-tube-playlist.herokuapp.com/pictures';
 export const videoURL = 'https://your-tube-playlist.herokuapp.com/videos';
 
 /*
-export const pictureURL = 'https://localhost:5000/posts';
-export const videoURL = 'https://localhost:5000/videos';
+export const pictureURL = 'http://localhost:5000/pictures';
+export const videoURL = 'http://localhost:5000/videos';
 */
